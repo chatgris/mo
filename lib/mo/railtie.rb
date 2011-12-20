936ab55b8aa1b32117b99075f8cdccf88440c16b
chatgris <jboyer@af83.com>
2011-12-20 22:27:30 +0100

    Added encoding task.
    Courtesy of changa. :)

--- lib/mo/railtie.rb
+++ lib/mo/railtie.rb
@@ -14,6 +14,32 @@ module Mo
             system "sed -e 's/[ \t]*$//' -i #{file}"
           end
         end
+
+        # Courtesy of changa.
+        desc "Add utf-8 encoding on files that don't have it"
+        task :encoding do
+          require 'find'
+          files_to_convert = []
+
+          Find.find Rails.root.to_s do |f|
+            if File.file?(f) && f =~ /\.(rb|feature|rake)$/ && `head -n 1 #{f}|grep '# encoding'`.empty?
+              files_to_convert << f
+            end
+          end
+
+          print "Converting #{files_to_convert.size} files"
+          puts if verbose
+          files_to_convert.each do |f|
+            system "sed -i -r '1 s/^(.*)$/# encoding: utf-8\\n\\1/g' #{f}"
+            if verbose
+              puts "  * #{f}"
+            else
+              print '.'
+            end
+          end
+          puts unless verbose
+
+        end
       end
     end
   end
